71de574951510529bb72da158b899ebe8109dd47
Kacper Madej <kacperoza@gmail.com>
2016-09-20 20:09:49 +0200

    Add pixi.js; start rewriting canvas to Pixi

--- webroot/js/game.js
+++ webroot/js/game.js
@@ -1,4 +1,8 @@
 import ProtoBuf from 'protobufjs';
+import * as PIXI from "pixi.js";
+
+const renderer = new PIXI.WebGLRenderer(800, 600);
+const stage = new PIXI.Container();
 
 const KEY_UP = 38
 const KEY_LEFT = 37
@@ -9,6 +13,7 @@ var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+document.body.appendChild(renderer.view);
 document.body.appendChild(canvas);
 
 var builder = ProtoBuf.loadJsonFile("js/superstellar_proto.json");
@@ -19,11 +24,27 @@ var UserInput = builder.build("superstellar.UserInput")
 var shipReady = false;
 var shipThrustReady = false;
 
-var shipImage = new Image();
-shipImage.onload = function () {
+var _shipImage = new Image();
+_shipImage.onload = function () {
   shipReady = true;
 };
-shipImage.src = "images/ship.png";
+_shipImage.src = "images/ship.png";
+
+PIXI.loader.add(["images/ship.png", "images/ship_thrust.png"]).load(setup);
+
+let shipSprite, shipThrustSprite;
+function setup() {
+  shipSprite = new PIXI.Sprite(PIXI.loader.resources["images/ship.png"].texture);
+  shipThrustSprite = new PIXI.Sprite(PIXI.loader.resources["images/ship_thrust.png"].texture);
+
+  stage.addChild(shipSprite);
+  shipSprite.x = renderer.width / 2;
+  shipSprite.y = renderer.height / 2;
+  shipSprite.rotation = 3 * Math.PI / 2;
+  shipSprite.pivot.set(shipSprite.width / 2, shipSprite.height / 2);
+  renderer.render(stage);
+}
+
 
 var shipThrustImage = new Image();
 shipThrustImage.onload = function () {
@@ -44,124 +65,127 @@ var lastTime = Date.now();
 var fps = 0;
 
 ws.onmessage = function(e) {
-	var fileReader = new FileReader();
-	fileReader.onload = function() {
+  var fileReader = new FileReader();
+  fileReader.onload = function() {
     ships = Space.decode(this.result).spaceships
 
     if (myID == 0) {
       for (var i in ships) {
-        if (ships[i].id > myID) {
-          myID = ships[i].id
-        }
+	if (ships[i].id > myID) {
+	  myID = ships[i].id
+	}
       }
     }
   };
 
-	fileReader.readAsArrayBuffer(e.data);
+  fileReader.readAsArrayBuffer(e.data);
 };
 
 // Handle keyboard controls
 var keysDown = {};
 
 addEventListener("keydown", function (e) {
-	keysDown[e.keyCode] = true;
+  keysDown[e.keyCode] = true;
 }, false);
 
 addEventListener("keyup", function (e) {
-	delete keysDown[e.keyCode];
+  delete keysDown[e.keyCode];
 }, false);
 
 var translateToViewport = function (x, y, viewport) {
   var newX = x - viewport.vx + viewport.width / 2;
-	var newY = -y + viewport.vy + viewport.height / 2;
-	return {x: newX, y: newY}
+  var newY = -y + viewport.vy + viewport.height / 2;
+  return {x: newX, y: newY}
 }
 
 var sendInput = function() {
-	var thrust = KEY_UP in keysDown;
+  var thrust = KEY_UP in keysDown;
 
-	var direction = "NONE";
-	if (KEY_LEFT in keysDown) {
-		direction = "LEFT";
-	} else if (KEY_RIGHT in keysDown) {
-		direction = "RIGHT";
-	}
+  var direction = "NONE";
+  if (KEY_LEFT in keysDown) {
+    direction = "LEFT";
+  } else if (KEY_RIGHT in keysDown) {
+    direction = "RIGHT";
+  }
 
-	var userInput = new UserInput(thrust, direction);
-	var buffer = userInput.encode();
+  var userInput = new UserInput(thrust, direction);
+  var buffer = userInput.encode();
 
-	if (ws.readyState == WebSocket.OPEN) {
-			ws.send(buffer.toArrayBuffer());
-	}
+  if (ws.readyState == WebSocket.OPEN) {
+    ws.send(buffer.toArrayBuffer());
+  }
 
 }
 
 // Draw everything
 var render = function () {
-	ctx.beginPath();
+  ctx.beginPath();
 
-	var myShip;
+  var myShip;
 
-	if (ships.length > 0) {
+  if (ships.length > 0) {
     myShip = ships.find(function(ship) { return ship.id == myID })
     var ownPosition = {x: myShip.position.x/100, y: myShip.position.y/100};
     viewport = {vx: ownPosition.x, vy: ownPosition.y, width: 800, height: 600};
   }
 
-	ctx.rect(0, 0, 800, 600);
-	ctx.fillStyle = "black";
-	ctx.fill();
+  ctx.rect(0, 0, 800, 600);
+  ctx.fillStyle = "black";
+  ctx.fill();
 
-	if (shipReady) {
-		for (var shipID in ships) {
-			var ship = ships[shipID]
+  if (shipReady) {
+    for (var shipID in ships) {
+      var ship = ships[shipID]
 
-			let image = ship.input_thrust ? shipThrustImage : shipImage
+      let image = ship.input_thrust ? shipThrustImage : _shipImage
 
-			var translatedPosition = translateToViewport(ship.position.x/100, ship.position.y/100, viewport)
+      var translatedPosition = translateToViewport(ship.position.x/100, ship.position.y/100, viewport)
 
-			ctx.translate(translatedPosition.x, translatedPosition.y);
-			ctx.fillStyle = "rgb(250, 250, 250)";
-			ctx.font = "18px Helvetica";
-			ctx.fillText(ship.id, -35, -60);
+      ctx.translate(translatedPosition.x, translatedPosition.y);
+      ctx.fillStyle = "rgb(250, 250, 250)";
+      ctx.font = "18px Helvetica";
+      ctx.fillText(ship.id, -35, -60);
 
-			ctx.rotate(ship.facing);
-			ctx.drawImage(image, -30, -22);
-			ctx.rotate(-ship.facing);
-			ctx.translate(-translatedPosition.x, -translatedPosition.y);
-		}
-	}
+      ctx.rotate(ship.facing);
+      shipSprite.rotation = ship.facing;
 
-	frameCounter++;
+      ctx.drawImage(image, -30, -22);
+      ctx.rotate(-ship.facing);
+      ctx.translate(-translatedPosition.x, -translatedPosition.y);
+    }
+  }
 
-	if (frameCounter === 100) {
-		frameCounter = 0;
-		var now = Date.now();
-		var delta = (now - lastTime) / 1000;
-		fps = (100 / delta).toFixed(1);
-		lastTime = now;
-	}
+  frameCounter++;
 
-	// Score
-	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
-	ctx.textAlign = "left";
-	ctx.textBaseline = "top";
-	ctx.fillText("Ships: " + ships.length, 580, 10);
-	ctx.fillText("FPS: " + fps, 580, 40);
-	if (undefined != myShip) {
+  if (frameCounter === 100) {
+    frameCounter = 0;
+    var now = Date.now();
+    var delta = (now - lastTime) / 1000;
+    fps = (100 / delta).toFixed(1);
+    lastTime = now;
+  }
+
+  // Score
+  ctx.fillStyle = "rgb(250, 250, 250)";
+  ctx.font = "24px Helvetica";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillText("Ships: " + ships.length, 580, 10);
+  ctx.fillText("FPS: " + fps, 580, 40);
+  if (undefined != myShip) {
     ctx.fillText("X: " + Math.floor(myShip.position.x / 100), 580, 70);
     ctx.fillText("Y: " + Math.floor(myShip.position.y / 100), 580, 100);
-	}
+  }
 
-	sendInput()
+  renderer.render(stage);
+  sendInput()
 };
 
 // The main game loop
 var main = function () {
-	render();
-	// Request to do this again ASAP
-	requestAnimationFrame(main);
+  render();
+  // Request to do this again ASAP
+  requestAnimationFrame(main);
 };
 
 // Let's play this game!
--- webroot/webpack.config.js
+++ webroot/webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
@@ -11,13 +12,28 @@ module.exports = {
     module: {
 	loaders: [
 	    {
+	    test: /\.json$/,
+	    loader: 'json'
+	},
+	{
 	    test: /\.js$/,
 	    exclude: /node_modules/,
 	    loader: 'babel'
+	},
+	{ 
+	    test: path.resolve(__dirname, 'node_modules', 'pixi.js'),
+	    loader: 'ify' 
 	}
 	]
     },
     plugins: [
 	new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.js")
+    ],
+    postLoaders: [
+	{
+	test: /\.js$/,
+	include: path.resolve(__dirname, 'node_modules/pixi.js'),
+	loader: 'transform/cacheable?brfs'
+    }
     ]
 };
