cdcd482b5b41a250e2293c410279623bc6c9b681
Michal Konarski <michal.konarski@u2i.com>
2016-12-03 01:40:21 +0100

    Revert "Revert 'Add dumping space communication to file'"
    This reverts commit 32228fed89567e8c6b2f537d8ea0256043ab8f06.

---
+++ backend/communication/file_writer.go
@@ -0,0 +1,95 @@
+package communication
+
+import (
+	"encoding/binary"
+	"github.com/golang/protobuf/proto"
+	"log"
+	"os"
+	"path"
+	"superstellar/backend/events"
+	"superstellar/backend/pb"
+	"superstellar/backend/state"
+	"time"
+)
+
+const (
+	DIRECTORY = "log"
+)
+
+type FileWriter struct {
+	file  *os.File
+	space *state.Space
+	ch    chan *pb.Space
+}
+
+func NewFileWriter(space *state.Space) (*FileWriter, error) {
+	file, err := openFile()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &FileWriter{
+		file:  file,
+		space: space,
+		ch:    make(chan *pb.Space, 100),
+	}, nil
+}
+
+func openFile() (*os.File, error) {
+	filename := time.Now().Format("2006-01-02_150405.log")
+
+	if err := os.MkdirAll(DIRECTORY, 0777); err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	filepath := path.Join(DIRECTORY, filename)
+	file, err := os.Create(filepath)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("log file created:", filepath)
+	}
+
+	return file, err
+}
+
+func (fileWriter *FileWriter) Run() {
+	defer fileWriter.close()
+
+	for {
+		protoSpace := <-fileWriter.ch
+		fileWriter.writeToFile(protoSpace)
+	}
+}
+
+func (fileWriter *FileWriter) writeToFile(space *pb.Space) error {
+	data, err := proto.Marshal(space)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	msgLen := uint32(len(data))
+	if binary.Write(fileWriter.file, binary.LittleEndian, msgLen); err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	if fileWriter.file.Write(data); err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	return nil
+}
+
+func (fileWriter *FileWriter) close() {
+	fileWriter.file.Close()
+}
+
+func (fileWriter *FileWriter) HandleCommunicationTimeTick(timeTickEvent *events.CommunicationTimeTick) {
+	fileWriter.ch <- fileWriter.space.ToProto()
+}
